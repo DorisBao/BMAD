fa9a412790a89c2cbf73aa40f4c95e0182bdcc14
preminstrel <preminstrel@gmail.com>
2023-07-05 13:41:39 +0800

    🚀 ADD Deep-SVDD

--- Deep-SVDD/main.py
+++ Deep-SVDD/main.py
@@ -8,6 +8,8 @@ from utils.config import Config
 from utils.visualization.plot_images_grid import plot_images_grid
 from deepSVDD import DeepSVDD
 from datasets.main import load_dataset
+import os
+import yaml
 
 
 ################################################################################
@@ -16,46 +18,45 @@ from datasets.main import load_dataset
 @click.command()
 @click.argument('dataset_name', type=click.Choice(['RESC', 'OCT2017', 'liver', 'bras2021', 'chest', 'camelyon']))
 @click.argument('net_name', type=click.Choice(['mnist_LeNet', 'cifar10_LeNet', 'cifar10_LeNet_ELU']))
-@click.argument('xp_path', type=click.Path(exists=True))
+@click.argument('xp_path')
 @click.argument('data_path', type=click.Path(exists=True))
 @click.option('--load_config', type=click.Path(exists=True), default=None,
               help='Config JSON-file path (default: None).')
 @click.option('--load_model', type=click.Path(exists=True), default=None,
               help='Model file path (default: None).')
-@click.option('--objective', type=click.Choice(['one-class', 'soft-boundary']), default='one-class',
-              help='Specify Deep SVDD objective ("one-class" or "soft-boundary").')
-@click.option('--nu', type=float, default=0.1, help='Deep SVDD hyperparameter nu (must be 0 < nu <= 1).')
+# @click.option('--objective', type=click.Choice(['one-class', 'soft-boundary']), default='one-class',
+#               help='Specify Deep SVDD objective ("one-class" or "soft-boundary").')
+# @click.option('--nu', type=float, default=0.1, help='Deep SVDD hyperparameter nu (must be 0 < nu <= 1).')
 @click.option('--device', type=str, default='cuda', help='Computation device to use ("cpu", "cuda", "cuda:2", etc.).')
-@click.option('--seed', type=int, default=-1, help='Set seed. If -1, use randomization.')
-@click.option('--optimizer_name', type=click.Choice(['adam', 'amsgrad']), default='adam',
-              help='Name of the optimizer to use for Deep SVDD network training.')
-@click.option('--lr', type=float, default=0.001,
-              help='Initial learning rate for Deep SVDD network training. Default=0.001')
-@click.option('--n_epochs', type=int, default=50, help='Number of epochs to train.')
-@click.option('--lr_milestone', type=int, default=0, multiple=True,
-              help='Lr scheduler milestones at which lr is multiplied by 0.1. Can be multiple and must be increasing.')
-@click.option('--batch_size', type=int, default=128, help='Batch size for mini-batch training.')
-@click.option('--weight_decay', type=float, default=1e-6,
-              help='Weight decay (L2 penalty) hyperparameter for Deep SVDD objective.')
+# @click.option('--seed', type=int, default=-1, help='Set seed. If -1, use randomization.')
+# @click.option('--optimizer_name', type=click.Choice(['adam', 'amsgrad']), default='adam',
+#               help='Name of the optimizer to use for Deep SVDD network training.')
+# @click.option('--lr', type=float, default=0.001,
+#               help='Initial learning rate for Deep SVDD network training. Default=0.001')
+# @click.option('--n_epochs', type=int, default=50, help='Number of epochs to train.')
+# @click.option('--lr_milestone', type=int, default=0, multiple=True,
+#               help='Lr scheduler milestones at which lr is multiplied by 0.1. Can be multiple and must be increasing.')
+# @click.option('--batch_size', type=int, default=128, help='Batch size for mini-batch training.')
+# @click.option('--weight_decay', type=float, default=1e-6,
+#               help='Weight decay (L2 penalty) hyperparameter for Deep SVDD objective.')
 @click.option('--pretrain', type=bool, default=True,
               help='Pretrain neural network parameters via autoencoder.')
-@click.option('--ae_optimizer_name', type=click.Choice(['adam', 'amsgrad']), default='adam',
-              help='Name of the optimizer to use for autoencoder pretraining.')
-@click.option('--ae_lr', type=float, default=0.001,
-              help='Initial learning rate for autoencoder pretraining. Default=0.001')
-@click.option('--ae_n_epochs', type=int, default=100, help='Number of epochs to train autoencoder.')
-@click.option('--ae_lr_milestone', type=int, default=0, multiple=True,
-              help='Lr scheduler milestones at which lr is multiplied by 0.1. Can be multiple and must be increasing.')
-@click.option('--ae_batch_size', type=int, default=128, help='Batch size for mini-batch autoencoder training.')
-@click.option('--ae_weight_decay', type=float, default=1e-6,
-              help='Weight decay (L2 penalty) hyperparameter for autoencoder objective.')
+# @click.option('--ae_optimizer_name', type=click.Choice(['adam', 'amsgrad']), default='adam',
+#               help='Name of the optimizer to use for autoencoder pretraining.')
+# @click.option('--ae_lr', type=float, default=0.001,
+#               help='Initial learning rate for autoencoder pretraining. Default=0.001')
+# @click.option('--ae_n_epochs', type=int, default=100, help='Number of epochs to train autoencoder.')
+# @click.option('--ae_lr_milestone', type=int, default=0, multiple=True,
+#               help='Lr scheduler milestones at which lr is multiplied by 0.1. Can be multiple and must be increasing.')
+# @click.option('--ae_batch_size', type=int, default=128, help='Batch size for mini-batch autoencoder training.')
+# @click.option('--ae_weight_decay', type=float, default=1e-6,
+#               help='Weight decay (L2 penalty) hyperparameter for autoencoder objective.')
 @click.option('--n_jobs_dataloader', type=int, default=0,
               help='Number of workers for data loading. 0 means that the data will be loaded in the main process.')
 @click.option('--normal_class', type=int, default=0,
               help='Specify the normal class of the dataset (all other classes are considered anomalous).')
-def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, objective, nu, device, seed,
-         optimizer_name, lr, n_epochs, lr_milestone, batch_size, weight_decay, pretrain, ae_optimizer_name, ae_lr,
-         ae_n_epochs, ae_lr_milestone, ae_batch_size, ae_weight_decay, n_jobs_dataloader, normal_class):
+
+def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, device, pretrain, n_jobs_dataloader, normal_class):
     """
     Deep SVDD, a fully deep method for anomaly detection.
 
@@ -64,8 +65,15 @@ def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, ob
     :arg XP_PATH: Export path for logging the experiment.
     :arg DATA_PATH: Root path of data.
     """
-
+    config_path = f"config/{dataset_name}_DeepSVDD.yaml"
+    print(f"reading config {config_path}...")
+    with open(config_path, 'r') as file:
+        config = yaml.safe_load(file)
+    print(config)
+    if os.path.exists(xp_path) !=True:
+        os.makedirs(xp_path)
     # Get configuration
+    
     cfg = Config(locals().copy())
 
     # Set up logging
@@ -94,15 +102,15 @@ def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, ob
         logger.info('Loaded configuration from %s.' % load_config)
 
     # Print configuration
-    logger.info('Deep SVDD objective: %s' % cfg.settings['objective'])
-    logger.info('Nu-paramerter: %.2f' % cfg.settings['nu'])
+    logger.info('Deep SVDD objective: %s' % config['objective'])
+    logger.info('Nu-paramerter: %.2f' % config['nu'])
 
     # Set seed
-    if cfg.settings['seed'] != -1:
-        random.seed(cfg.settings['seed'])
-        np.random.seed(cfg.settings['seed'])
-        torch.manual_seed(cfg.settings['seed'])
-        logger.info('Set seed to %d.' % cfg.settings['seed'])
+    if config['seed'] != -1:
+        random.seed(config['seed'])
+        np.random.seed(config['seed'])
+        torch.manual_seed(config['seed'])
+        logger.info('Set seed to %d.' % config['seed'])
 
     # Default device to 'cpu' if cuda is not available
     if not torch.cuda.is_available():
@@ -119,7 +127,7 @@ def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, ob
     # valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=11164, num_workers=8)
 
     # Initialize DeepSVDD model and set neural network \phi
-    deep_SVDD = DeepSVDD(cfg.settings['objective'], cfg.settings['nu'])
+    deep_SVDD = DeepSVDD(config['objective'], config['nu'])
     deep_SVDD.set_network(net_name)
     # If specified, load Deep SVDD model (radius R, center c, network weights, and possibly autoencoder weights)
     if load_model:
@@ -129,40 +137,41 @@ def main(dataset_name, net_name, xp_path, data_path, load_config, load_model, ob
     logger.info('Pretraining: %s' % pretrain)
     if pretrain:
         # Log pretraining details
-        logger.info('Pretraining optimizer: %s' % cfg.settings['ae_optimizer_name'])
-        logger.info('Pretraining learning rate: %g' % cfg.settings['ae_lr'])
-        logger.info('Pretraining epochs: %d' % cfg.settings['ae_n_epochs'])
-        logger.info('Pretraining learning rate scheduler milestones: %s' % (cfg.settings['ae_lr_milestone'],))
-        logger.info('Pretraining batch size: %d' % cfg.settings['ae_batch_size'])
-        logger.info('Pretraining weight decay: %g' % cfg.settings['ae_weight_decay'])
+        logger.info('Pretraining optimizer: %s' % config['ae_optimizer_name'])
+        logger.info('Pretraining learning rate: %g' % config['ae_lr'])
+        logger.info('Pretraining epochs: %d' % config['ae_n_epochs'])
+        logger.info('Pretraining learning rate scheduler milestones: %s' % (config['ae_lr_milestone'],))
+        logger.info('Pretraining batch size: %d' % config['ae_batch_size'])
+        logger.info('Pretraining weight decay: %g' % config['ae_weight_decay'])
 
+        #print(cfg.settings['ae_lr_milestone'], config['ae_lr_milestone'])
         # Pretrain model on dataset (via autoencoder)
         deep_SVDD.pretrain(train_dataset,
-                           optimizer_name=cfg.settings['ae_optimizer_name'],
-                           lr=cfg.settings['ae_lr'],
-                           n_epochs=cfg.settings['ae_n_epochs'],
-                           lr_milestones=cfg.settings['ae_lr_milestone'],
-                           batch_size=cfg.settings['ae_batch_size'],
-                           weight_decay=cfg.settings['ae_weight_decay'],
+                           optimizer_name=config['ae_optimizer_name'],
+                           lr=config['ae_lr'],
+                           n_epochs=config['ae_n_epochs'],
+                           lr_milestones=(config['ae_lr_milestone'],),
+                           batch_size=config['ae_batch_size'],
+                           weight_decay=config['ae_weight_decay'],
                            device=device,
                            n_jobs_dataloader=n_jobs_dataloader)
 
     # Log training details
-    logger.info('Training optimizer: %s' % cfg.settings['optimizer_name'])
-    logger.info('Training learning rate: %g' % cfg.settings['lr'])
-    logger.info('Training epochs: %d' % cfg.settings['n_epochs'])
-    logger.info('Training learning rate scheduler milestones: %s' % (cfg.settings['lr_milestone'],))
-    logger.info('Training batch size: %d' % cfg.settings['batch_size'])
-    logger.info('Training weight decay: %g' % cfg.settings['weight_decay'])
+    logger.info('Training optimizer: %s' % config['optimizer_name'])
+    logger.info('Training learning rate: %g' % config['lr'])
+    logger.info('Training epochs: %d' % config['n_epochs'])
+    logger.info('Training learning rate scheduler milestones: %s' % (config['lr_milestone'],))
+    logger.info('Training batch size: %d' % config['batch_size'])
+    logger.info('Training weight decay: %g' % config['weight_decay'])
 
     # Train model on dataset
     deep_SVDD.train(train_dataset,
-                    optimizer_name=cfg.settings['optimizer_name'],
-                    lr=cfg.settings['lr'],
-                    n_epochs=cfg.settings['n_epochs'],
-                    lr_milestones=cfg.settings['lr_milestone'],
-                    batch_size=cfg.settings['batch_size'],
-                    weight_decay=cfg.settings['weight_decay'],
+                    optimizer_name=config['optimizer_name'],
+                    lr=config['lr'],
+                    n_epochs=config['n_epochs'],
+                    lr_milestones=(config['lr_milestone'],),
+                    batch_size=config['batch_size'],
+                    weight_decay=config['weight_decay'],
                     device=device,
                     n_jobs_dataloader=n_jobs_dataloader)
 
--- main.py
+++ main.py
@@ -21,8 +21,10 @@ if __name__ == '__main__':
     if mode == 'train':
         if model in ["padim", "padim_resnet50", "stfpm", "stfpm_resnet50","draem", "cfa", "cflow", "ganomaly", "RD4AD", "patchcore", "patchcore_resnet50"]:
             os.system(f'python anomalib/tools/train.py --config config/{data}_{model}.yaml')
-        elif mode == "Deep-SVDD":
-            os.system(f"python Deep-SVDD/main.py {data} cifar10_LeNet Deep-SVDD/log/{data} /home/jinan/ --objective one-class --lr 0.0001 --n_epochs 300 --lr_milestone 50 --batch_size 200 --weight_decay 0.5e-7 --pretrain True --ae_lr 0.00001 --ae_n_epochs 300 --ae_lr_milestone 250 --ae_batch_size 200 --ae_weight_decay 0.5e-6 --normal_class 3")
+        elif model == "Deep-SVDD":
+            os.system(f"python Deep-SVDD/main.py {data} cifar10_LeNet Deep-SVDD/log/{data} /home/jinan/")
+        elif model == "UTRAD":
+            os.system(f"pwd")
     elif mode == 'test':
         if model in ["padim", "padim_resnet50", "stfpm", "stfpm_resnet50","draem", "cfa", "cflow", "ganomaly", "RD4AD", "patchcore", "patchcore_resnet50"]:
             os.system(f'python anomalib/tools/test.py --model {model} --config config/{data}_{model}_test.yaml --weight_file {args.weight}')
